4d8fbed01dd9dc5a539d8c59b3ae0692ce7ee548
Maghraby <maghraby.fcih@gmail.com>
2016-07-11 03:37:55 +0200

    changes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { AppComponent, environment } from './app/';
+import { TodoAppComponent, environment } from './app/';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
-
+bootstrap(TodoAppComponent);
